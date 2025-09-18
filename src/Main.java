94562f268bb2087989d0449363234724b04829bb
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-09-18 23:21:53 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("🚀 Hello from Java inside Docker!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Main implements RequestHandler<S3Event, String> {
+
+    @Override
+    public String handleRequest(S3Event event, Context context) {
+        context.getLogger().log("🚀 Lambda triggered by S3 event!\n");
+
+        event.getRecords().forEach(record -> {
+            String bucket = record.getS3().getBucket().getName();
+            String key = record.getS3().getObject().getKey();
+            context.getLogger().log("📦 File uploaded: " + bucket + "/" + key + "\n");
+        });
+
+        return "✅ Lambda executed successfully!";
     }
 }
